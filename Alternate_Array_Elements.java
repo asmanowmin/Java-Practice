762306ba971b03516e678d01f9ae440202d79f74
asmanowmin <124590717+asmanowmin@users.noreply.github.com>
2026-01-11 22:39:58 +0530

    Alternate_Array_Elements.java

---
+++ Alternate_Array_Elements.java
@@ -0,0 +1,15 @@
+class AlternateArrayElements{
+    public static void main(String[]args)
+    {
+        int n = 5;
+        int [] a = {1,2,3,4,5};
+        
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
