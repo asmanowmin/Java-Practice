835dfd28dc728c9210bbab94e25883c9af08a76d
asmanowmin <124590717+asmanowmin@users.noreply.github.com>
2026-01-11 20:27:01 +0530

    Count_squares.java

---
+++ Count_squares.java
@@ -0,0 +1,12 @@
+public class Count_Squares {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int [] a = {1,2,3,4,5};
+        int count = "";
+        for (int i = 0; i <= Math.sqrt(a[i]); i++) { 
+            count++;
+        }
+        System.out.println(count);
+    }
+}
